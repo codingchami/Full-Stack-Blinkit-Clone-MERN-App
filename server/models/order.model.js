50dbde2e0807758f269b0f448501d74afe960059
codingchami <cschamithsandeepa@gmail.com>
2025-02-25 19:46:15 +0530

    created the Order Schema successfully.

---
+++ server/models/order.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    user_Id : {
+        type : mongoose.Schema.ObjectId,
+        ref : "User"
+    },
+    ordeId : {
+        type : String,
+        required : [true, "Provide orderId"],
+        unique : true
+    },
+    productId : {
+        type : mongoose.Schema.ObjectId,
+        ref : "product"
+    },
+    product_details : {
+        _id : String,
+        name : String,
+        image : Array,
+
+    },
+    paymentId : {
+        type : String,
+        default : ""
+    },
+    payment_status : {
+        type : String,
+        default : ""
+    },
+    delivery_address : {
+        type : mongoose.Schema.ObjectId,
+        ref : "address"
+    },
+    subTotal : {
+        type : Number,
+        default : 0
+    },
+    totalAmount : {
+        type : Number,
+        default : 0
+    },
+    invoice_receipt : {
+        type : String,
+        default : ""
+    }
+})
