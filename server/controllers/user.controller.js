54b53d5fa66333ddb38645341ff921ce9ea31f61
codingchami <cschamithsandeepa@gmail.com>
2025-03-03 13:36:11 +0530

    add the proper message when user enter only the email or password

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -105,6 +105,14 @@ export async function loginController(request, response) {
     try {
         const { email, password } = request.body
 
+        if(!email || !password){
+            return response.status(400).json({
+                message: "Provide email and password",
+                error: true,
+                success: false
+            })
+        }
+
         const user = await UserModel.findOne({ email })
 
         if (!user) {
