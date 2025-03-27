837169a5a0ae9dfba96385b59dfa5949305a131d
codingchami <cschamithsandeepa@gmail.com>
2025-03-27 16:50:19 +0530

    develop search bar using typeAnimation

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
+import { TypeAnimation } from 'react-type-animation';
 
 const Search = () => {
     return (
-        <div className='w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500'>
+        <div className='w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-200'>
             <button className='flex justify-center items-center h-full p-3'>
-                <IoSearch  size={22}/>
+                <IoSearch size={22} />
             </button>
             <div>
-                Search "Milk"
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'Search "Milk"',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'Search "bread"',
+                        1000,
+                        'Search "Sugar"',
+                        1000,
+                        'Search "Soap"',
+                        1000,
+                        'Search "chocolate"',
+                        1000,
+                        'Search "eggs"',
+                        1000,
+                        'Search "Chips"',
+                        1000,
+                        'Search "rice"',
+                        1000,
+                        'Search "yoghurt"',
+                        1000,
+                        'Search "flour"',
+                        1000,
+                        'Search "butter"',
+                        1000,
+                        'Search "cheese"',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    repeat={Infinity}
+                />
             </div>
         </div>
     )
