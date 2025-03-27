84790240a5ad4bd26c1511a7635805e2dfa03895
codingchami <cschamithsandeepa@gmail.com>
2025-03-27 16:27:48 +0530

    create the basic of the header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import Search from './Search'
 
 const Header = () => {
-  return (
-    <div>
-        Header
-    </div>
-  )
+    return (
+        <header className='h-20 shadow-md sticky top-0'>
+            <div className='container mx-auto flex items-center h-full PX-2 justify-between'>
+                {/**logo */}
+                <div className='h-full'>
+                    <div className='h-full flex justify-center items-center'>
+                        <img src={logo}
+                            width={170}
+                            height={60}
+                            alt="logo"
+                            className='hidden lg:block'
+                        />
+                         <img src={logo}
+                            width={120}
+                            height={60}
+                            alt="logo"
+                            className='lg:hidden'
+
+                        />
+                    </div>
+                </div>
+
+                {/**Search */}
+                <div>
+                    <Search />
+                </div>
+
+                {/**login and my cart */}
+                <div>
+                    Login and my cart
+                </div>
+            </div>
+        </header>
+
+    )
 }
 
 export default Header
