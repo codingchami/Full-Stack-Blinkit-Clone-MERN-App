50a67c2eea669819b0c43c923c237da17ceaaa75
codingchami <cschamithsandeepa@gmail.com>
2025-03-27 10:48:07 +0530

    set the react-router-dom successfully.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <main>
+    <main className='text-red-600 text-4xl font-bold'>
       Vite App
       <h1 className="text-6xl font-bold text-blue-600">Tailwind CSS is working!</h1>
     </main>
@@ -11,3 +11,4 @@ function App() {
 }
 
 export default App
+s
