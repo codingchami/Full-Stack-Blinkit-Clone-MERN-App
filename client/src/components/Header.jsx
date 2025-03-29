b6940105698e3ea9fefceff17c2db8ea2463be61
codingchami <cschamithsandeepa@gmail.com>
2025-03-29 17:08:26 +0530

    added more proper styles

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import Search from './Search'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
@@ -8,7 +9,7 @@ const Header = () => {
             <div className='container mx-auto flex items-center h-full PX-2 justify-between'>
                 {/**logo */}
                 <div className='h-full'>
-                    <div className='h-full flex justify-center items-center'>
+                    <Link to={"/"} className='h-full flex justify-center items-center'>
                         <img src={logo}
                             width={170}
                             height={60}
@@ -22,7 +23,7 @@ const Header = () => {
                             className='lg:hidden'
 
                         />
-                    </div>
+                    </Link>
                 </div>
 
                 {/**Search */}
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,47 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoSearch } from "react-icons/io5";
+import { useLocation, useNavigate } from 'react-router-dom';
 import { TypeAnimation } from 'react-type-animation';
 
 const Search = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const [isSearchPage, setIsSearchPage] = useState(false)
+
+    useEffect(() => {
+        const isSearch = location.pathname === '/search'
+        setIsSearchPage(isSearch)
+    }, [location])
+
+    console.log("location", location)
+
+    const redirectToSearchPage = () => {
+        navigate('/search')
+    }
     return (
         <div className='w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-200'>
             <button className='flex justify-center items-center h-full p-3'>
                 <IoSearch size={22} />
             </button>
-            <div>
-                <TypeAnimation
-                    sequence={[
-                        // Same substring at the start will only be typed out once, initially
-                        'Search "Milk"',
-                        1000, // wait 1s before replacing "Mice" with "Hamsters"
-                        'Search "bread"',
-                        1000,
-                        'Search "Sugar"',
-                        1000,
-                        'Search "Soap"',
-                        1000,
-                        'Search "chocolate"',
-                        1000,
-                        'Search "eggs"',
-                        1000,
-                        'Search "Chips"',
-                        1000,
-                        'Search "rice"',
-                        1000,
-                        'Search "yoghurt"',
-                        1000,
-                        'Search "flour"',
-                        1000,
-                        'Search "butter"',
-                        1000,
-                        'Search "cheese"',
-                        1000
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    repeat={Infinity}
-                />
+            <div  className= 'w-full'>
+                {
+                    !isSearchPage ? (
+                        //not in search page
+                        <div onClick={redirectToSearchPage}>
+                            <TypeAnimation
+                                sequence={[
+                                    // Same substring at the start will only be typed out once, initially
+                                    'Search "Milk"',
+                                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                    'Search "bread"',
+                                    1000,
+                                    'Search "Sugar"',
+                                    1000,
+                                    'Search "Soap"',
+                                    1000,
+                                    'Search "chocolate"',
+                                    1000,
+                                    'Search "eggs"',
+                                    1000,
+                                    'Search "Chips"',
+                                    1000,
+                                    'Search "rice"',
+                                    1000,
+                                    'Search "yoghurt"',
+                                    1000,
+                                    'Search "flour"',
+                                    1000,
+                                    'Search "butter"',
+                                    1000,
+                                    'Search "cheese"',
+                                    1000
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                repeat={Infinity}
+                            />
+                        </div>
+                    ) : (
+                        //when i was search page
+                        <div className='w-full'>
+                            <input type="text" placeholder="Search for atta dal and more."
+                            className=''
+                            />
+                        </div>
+                    )
+                }
             </div>
+
         </div>
     )
 }
