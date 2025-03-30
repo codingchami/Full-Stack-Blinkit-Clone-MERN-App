44f02686abd6c9dd38769dd6de0e746bc7eafaa0
codingchami <cschamithsandeepa@gmail.com>
2025-03-31 00:08:55 +0530

    done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import Search from './Search'
 import { Link } from 'react-router-dom'
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Header = () => {
     return (
-        <header className='h-20 shadow-md sticky top-0'>
-            <div className='container mx-auto flex items-center h-full PX-2 justify-between'>
+        <header className='h-28 lg:h-20 lg:shadow-md sticky top-0 bg-red-500 flex items-center flex-col justify-center gap-1'>
+            <div className='container mx-auto flex items-center PX-2 justify-between'>
                 {/**logo */}
                 <div className='h-full'>
                     <Link to={"/"} className='h-full flex justify-center items-center'>
@@ -33,10 +34,16 @@ const Header = () => {
 
                 {/**login and my cart */}
                 <div className=''>
+                    <button className='text-neutral-600'>
+                        <FaRegUserCircle size={26}/>
+                    </button>
                     <div className='hidden lg:block'>
                         Login and my cart
                     </div>
                 </div>
+            </div>
+            <div className='container mx-auto px-2'>
+                <Search />
             </div>
         </header>
 
